b793932826e3a5ffcd88801cf90d50a85356920a
tear-wy <57802250+tear-wy@users.noreply.github.com>
2020-08-02 13:58:35 +0800

    首页-重新设置轮播、分类导航、资讯

--- Wechatapplet/pages/index/index.js
+++ Wechatapplet/pages/index/index.js
@@ -4,51 +4,47 @@ const app = getApp()
 
 Page({
   data: {
-    motto: '',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    swiper_imgs: [{
+        id: 0,
+        item_img_url: "../../icon/swiper-Icon/swiper-img1.jpg",
+      },
+      {
+        id: 1,
+        item_img_url: "../../icon/swiper-Icon/swiper-img2.jpg",
+      },
+      {
+        id: 2,
+        item_img_url: "../../icon/swiper-Icon/swiper-img3.jpg",
+      },
+    ],
+
+    classfy: [{
+        id: 0,
+        turn_url: "../wiki/wiki",
+        item_img_url: "../../icon/classfy-Icon/Img0.png",
+        item_name: "药物查询",
+      },
+      {
+        id: 1,
+        turn_url: "../inquiry/inquiry",
+        item_img_url: "../../icon/classfy-Icon/Img1.png",
+        item_name: "在线问诊",
+      },
+      {
+        id: 2,
+        turn_url: "./register/register",
+        item_img_url: "../../icon/classfy-Icon/Img2.png",
+        item_name: "急速挂号",
+      },
+      {
+        id: 3,
+        turn_url: "./calendar/calendar",
+        item_img_url: "../../icon/classfy-Icon/Img3.png",
+        item_name: "健康日历",
+      },
+    ]
+
+
   },
   //事件处理函数
-  bindViewTap: function () {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
