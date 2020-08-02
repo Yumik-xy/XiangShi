49d62d71e69c4a4daf26be1ed201af48e0b7f783
宇海 <44742784+Yumik-xy@users.noreply.github.com>
2020-08-03 00:02:33 +0800

    az

--- Django/api/admin.py
+++ Django/api/admin.py
@@ -3,12 +3,19 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib.auth.models import User
 from api.models import medicine
-
+from api.models import patient
 
 class medicineAdmin(admin.ModelAdmin):
     search_fields = ('drumname',)  # 根据属性搜索
     list_display = ('drumname',)  # 列表显示的属性
     pass
 
+class patientAdmin(admin.ModelAdmin):
+    search_fields = ('patientname',)  # 根据属性搜索
+    list_display = ('patientname',)  # 列表显示的属性
+    pass
+
 
 admin.site.register(medicine, medicineAdmin)
+
+admin.site.register(patient, patientAdmin)
