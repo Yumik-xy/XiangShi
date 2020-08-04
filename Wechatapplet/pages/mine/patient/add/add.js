c38cc3b246591b5ccfc7bace7bdcd7e0e094eeef
Alaskan233 <65169013+Alaskan233@users.noreply.github.com>
2020-08-04 14:35:23 +0800

    修改

--- Wechatapplet/pages/mine/patient/add/add.js
+++ Wechatapplet/pages/mine/patient/add/add.js
@@ -14,51 +14,107 @@ Page({
     console.log(e.detail.value)
     wx.login({
       success: function (loginCode) {
-        wx.request({
-          url: 'http://127.0.0.1/api/patient/',
-          data: {
-            patientname: e.detail.value.patientname,
-            gender: e.detail.value.gender,
-            age: e.detail.value.age,
-            telephone: e.detail.value.telephone,
-            pastmedicalhistory: e.detail.value.pastmedicalhistory,
-            telephone: e.detail.value.telephone,
-            allergy: e.detail.value.allergy,
-            coder: loginCode.code
-          },
-          header: { "content-type": "application/x-www-form-urlencoded" },
-          method: 'POST',
-          success: function (res) {
-            console.log(res)
-            if (res.data.status == false) {
-              wx.showToast({
-                title: res.data.message,
-                icon: 'none'
-              })
-            } else if (res.data.status == true) {
-              wx.showToast({
-                title: '添加成功',
-              })
+        if(this.data.is == 1){ 
+          wx.request({
+            url: 'http://127.0.0.1/api/patient/',
+            data: {
+              patientname: e.detail.value.patientname,
+              gender: e.detail.value.gender,
+              age: e.detail.value.age,
+              telephone: e.detail.value.telephone,
+              pastmedicalhistory: e.detail.value.pastmedicalhistory,
+              telephone: e.detail.value.telephone,
+              allergy: e.detail.value.allergy,
+              coder: loginCode.code
+            },
+            header: { "content-type": "application/x-www-form-urlencoded" },
+            method: 'POST',
+            success: function (res) {
+              console.log(res)
+              if (res.data.status == false) {
+                wx.showToast({
+                  title: res.data.message,
+                  icon: 'none'
+                })
+              } else if (res.data.status == true) {
+                wx.showToast({
+                  title: '添加成功',
+                })
 
-              that.setData({
-                save_data: e.detail.value
-              })
+                that.setData({
+                  save_data: e.detail.value
+                })
 
-              //获取存储数据的数组
-              var exprs = wx.getStorageSync("patientInfo") || []
-              //向数组中添加新的元素
-              exprs.unshift(that.data.save_data)
-              //将添加的元素存储到本地
-              wx.setStorageSync("patientInfo", exprs)
+                //获取存储数据的数组
+                var exprs = wx.getStorageSync("patientInfo") || []
+                //向数组中添加新的元素
+                exprs.unshift(that.data.save_data)
+                //将添加的元素存储到本地
+                wx.setStorageSync("patientInfo", exprs)
 
-              //获取缓存数据
-              var exprs = wx.getStorageSync("save_array") || []
-              that.setData({
-                arry_data: exprs
-              })
+                //获取缓存数据
+                var exprs = wx.getStorageSync("save_array") || []
+                that.setData({
+                  arry_data: exprs
+                })
+              }
             }
-          }
-        })
+          })
+        }
+        else{
+          wx.request({
+            url: 'http://127.0.0.1/api/patient/',
+            data: {
+              patientname: e.detail.value.patientname,
+              gender: e.detail.value.gender,
+              age: e.detail.value.age,
+              telephone: e.detail.value.telephone,
+              pastmedicalhistory: e.detail.value.pastmedicalhistory,
+              telephone: e.detail.value.telephone,
+              allergy: e.detail.value.allergy,
+              _patientname: info.patientname,
+              _gender: info.gender,
+              _age: info.age,
+              _telephone: info.telephone,
+              _pastmedicalhistory: info.pastmedicalhistory,
+              _telephone: info.telephone,
+              _allergy: info.allergy,
+              coder: loginCode.code
+            },
+            header: { "content-type": "application/x-www-form-urlencoded" },
+            method: 'PUT',
+            success: function (res) {
+              console.log(res)
+              if (res.data.status == false) {
+                wx.showToast({
+                  title: res.data.message,
+                  icon: 'none'
+                })
+              } else if (res.data.status == true) {
+                wx.showToast({
+                  title: '修改成功',
+                })
+  
+                that.setData({
+                  save_data: e.detail.value
+                })
+  
+                //获取存储数据的数组
+                var exprs = wx.getStorageSync("patientInfo") || []
+                //向数组中添加新的元素
+                exprs.unshift(that.data.save_data)
+                //将添加的元素存储到本地
+                wx.setStorageSync("patientInfo", exprs)
+  
+                //获取缓存数据
+                var exprs = wx.getStorageSync("save_array") || []
+                that.setData({
+                  arry_data: exprs
+                })
+              }
+            }
+          })
+        }
       }
     })
   },
@@ -69,7 +125,7 @@ Page({
   onLoad: function (options) {
     console.log(options)
     if (options.is == '1') {
-      this.setData({ info: JSON.parse(options.info), is: 1 });
+      this.setData({ info: JSON.parse(options.info), is: 1});
       console.log(this.data.info)
     }
   },
