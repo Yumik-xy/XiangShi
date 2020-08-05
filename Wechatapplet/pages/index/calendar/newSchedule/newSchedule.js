f5195f085aa60aaf5e8dc6f185e3b1c31b0f1b52
tear-wy <57802250+tear-wy@users.noreply.github.com>
2020-08-05 22:12:05 +0800

    日历-新增日程

--- Wechatapplet/pages/index/calendar/newSchedule/newSchedule.js
+++ Wechatapplet/pages/index/calendar/newSchedule/newSchedule.js
@@ -11,13 +11,44 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    timeCurrent: "12:00", //当前时间
+    timeBegin: "12:00",
+    timeEnd: "12:00",
+    title: "ni",
+    content: ""
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    onLoad: function (e) {
 
+    },
+    formSubmit: function (e) {
+      console.log('form发生了submit事件，携带数据为：', e.detail.value);
+      if (e.detail.value.title == "") {
+        wx: wx.showToast({
+          title: '标题为空',
+          icon: "loading"
+        });
+        return;
+      }
+      title = e.detail.value.title;
+      timeBegin = e.detail.value.timeBegin;
+      timeEnd = e.detail.value.timeEnd;
+      content = e.detail.value.content;
+    },
+
+    timeBeginChange: function (e) {
+      this.setData({
+        timeBegin: e.detail.value
+      })
+    },
+    timeEndChange: function (e) {
+      this.setData({
+        timeEnd: e.detail.value
+      })
+    }
   }
 })
