9081f9a49149a33bbc03a283b62d70c01abe7177
Alaskan233 <65169013+Alaskan233@users.noreply.github.com>
2020-08-10 15:58:32 +0800

    评论帖子

--- Wechatapplet/pages/inquiry/detail/detail.js
+++ Wechatapplet/pages/inquiry/detail/detail.js
@@ -1,18 +1,88 @@
 // pages/inquiry/detail/detail.js
+const app = getApp()
+
 Page({
   /**
    * 页面的初始数据
    */
   data: {
+    userInfo: {},
+    hasUserInfo: false,
     data: {
       photo: '../../../icon/add.png',
       follownum: 0,
       lovenum: 0,
     },
-    list : [],
-    picturelist: []
+    list: [],
+    picturelist: [],
+    reply: {
+      name: '',
+      postid: '',
+      parentid: '',
+      reply_to: ''
+    },
+    textareaisShow: false,
+    otherisShow: true
   },
-  
+
+  submit: function (e) {
+    var that = this
+    console.log(e.detail.value)
+    wx.login({
+      success: function (loginCode) {
+        wx.request({
+          url: 'http://127.0.0.1/api/comment',
+          data: {
+            name: that.data.reply.name,
+            postid: that.data.reply.postid,
+            parentid: that.data.reply.parentid,
+            reply_to: that.data.reply.reply_to,
+            body: e.detail.value.content,
+            coder: loginCode.code
+          },
+          header: {
+            "content-type": "application/x-www-form-urlencoded"
+          },
+          method: 'POST',
+          success: function (res) {
+            console.log(res)
+            if (res.data.status == false) {
+              wx.showToast({
+                title: res.data.message,
+                icon: 'none'
+              })
+            } else if (res.data.status == true) {
+              wx.showToast({
+                title: '发送成功',
+                duration: 1000,
+              })
+              setTimeout(function () {
+                wx.navigateBack({
+                  delta: 1
+                })
+              }, 1000)
+            }
+          }
+        })
+      }
+    })
+  },
+
+  bindcomment: function (e) {
+    let replyList = this.data.reply
+    replyList.name = this.data.userInfo.nickName,
+    replyList.postid = this.data.data.id,
+    replyList.parentid = e.currentTarget.dataset.id,
+    replyList.reply_to = e.currentTarget.dataset.name,
+
+      console.log(replyList);
+    this.setData({
+      reply: replyList,
+      textareaisShow: true,
+      otherisShow: false
+    })
+  },
+
   arrayToTree: function (arr, parent) {
     //  arr 是返回的数据  parendId 父id
     var that = this;
@@ -31,20 +101,20 @@ Page({
     return temp;
   },
 
-  previewPic: function(e) {
+  previewPic: function (e) {
     if (this.data.data.picture1.length > 0) {
       this.setData({
-        'picturelist[0]':('http://127.0.0.1/media/'+this.data.data.picture1)
+        'picturelist[0]': ('http://127.0.0.1/media/' + this.data.data.picture1)
       })
     }
     if (this.data.data.picture2.length > 0) {
       this.setData({
-        'picturelist[1]':('http://127.0.0.1/media/'+this.data.data.picture2)
+        'picturelist[1]': ('http://127.0.0.1/media/' + this.data.data.picture2)
       })
     }
     if (this.data.data.picture3.length > 0) {
       this.setData({
-        'picturelist[2]':('http://127.0.0.1/media/'+this.data.data.picture3)
+        'picturelist[2]': ('http://127.0.0.1/media/' + this.data.data.picture3)
       })
     }
 
@@ -59,36 +129,60 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+
     var that = this;
     this.setData({
       id: options.id
     })
     console.log(options.id)
 
-    wx.login({
-      success: function (loginCode) {
-        console.log(loginCode)
-        wx.request({
-          url: 'http://127.0.0.1/api/comment',
-          data: {
-            postid :that.data.id,
-          },
-          header: { "content-type": "application/x-www-form-urlencoded" },
-          method: 'GET',
-          success: function (res) {
-            console.log(res)
-            if (res.data.status == false) {
-              wx.showToast({
-                title: res.data.message,
-                icon: 'none'
-              })
-            } else if (res.data.status == true) {
-              that.setData({
-                list: that.arrayToTree(res.data.data,null)
-              })
-            }
-          }
-        })
+    wx.request({
+      url: 'http://127.0.0.1/api/comment',
+      data: {
+        postid: that.data.id,
+      },
+      header: {
+        "content-type": "application/x-www-form-urlencoded"
+      },
+      method: 'GET',
+      success: function (res) {
+        console.log(res)
+        if (res.data.status == false) {
+          wx.showToast({
+            title: res.data.message,
+            icon: 'none'
+          })
+        } else if (res.data.status == true) {
+          that.setData({
+            list: that.arrayToTree(res.data.data, null)
+          })
+        }
       }
     })
 
@@ -98,10 +192,12 @@ Page({
         wx.request({
           url: 'http://127.0.0.1/api/inquirypost',
           data: {
-            id : that.data.id,
-            coder:loginCode.code
+            id: that.data.id,
+            coder: loginCode.code
           },
-          header: { "content-type": "application/x-www-form-urlencoded" },
+          header: {
+            "content-type": "application/x-www-form-urlencoded"
+          },
           method: 'GET',
           success: function (res) {
             console.log(res)
@@ -111,8 +207,13 @@ Page({
                 icon: 'none'
               })
             } else if (res.data.status == true) {
+              var tempList = res.data.data[0]
+              if (tempList.time != null) {
+                tempList.date = tempList.time.substring(0, 10)
+                tempList.time = tempList.time.substring(11, 19); //要截取字段的字符串
+              }
               that.setData({
-                data: res.data.data[0],
+                data: tempList
               })
             }
           }
