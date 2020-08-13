19bd13cbdc86565560854f784ef8cbf72cc27230
zdg <1342230493@qq.com>
2020-08-13 16:44:46 +0800

    change_time

--- Wechatapplet/pages/inquiry/detail/detail.js
+++ Wechatapplet/pages/inquiry/detail/detail.js
@@ -94,6 +94,10 @@ Page({
                       icon: 'none'
                     })
                   } else if (res.data.status == true) {
+                    var tempList = res.data.data
+                    tempList.forEach((data) => {
+                      data.created = that.getDateDiff(data.created)
+                    })
                     that.setData({
                       list: that.arrayToTree(res.data.data, null)
                     })
@@ -259,6 +263,10 @@ Page({
             icon: 'none'
           })
         } else if (res.data.status == true) {
+          var tempList = res.data.data
+          tempList.forEach((data) => {
+            data.created = that.getDateDiff(data.created)
+          })
           that.setData({
             list: that.arrayToTree(res.data.data, null)
           })
@@ -267,6 +275,38 @@ Page({
     })
 
 
+  },
+  getDateDiff: function (dateTimeStamp) {
+    var result
+    var minute = 1000 * 60;
+    var hour = minute * 60;
+    var day = hour * 24;
+    var month = day * 30;
+    var now = Date.parse(new Date());
+    var diffValue = now - dateTimeStamp * 1000;
+    if (diffValue < 0) { return; }
+    var monthC = diffValue / month;
+    var weekC = diffValue / (7 * day);
+    var dayC = diffValue / day;
+    var hourC = diffValue / hour;
+    var minC = diffValue / minute;
+    if (monthC >= 1) {
+      result = "" + parseInt(monthC) + "月前";
+    }
+    else if (weekC >= 1) {
+      result = "" + parseInt(weekC) + "周前";
+    }
+    else if (dayC >= 1) {
+      result = "" + parseInt(dayC) + "天前";
+    }
+    else if (hourC >= 1) {
+      result = "" + parseInt(hourC) + "小时前";
+    }
+    else if (minC >= 1) {
+      result = "" + parseInt(minC) + "分钟前";
+    } else
+      result = "刚刚";
+    return result;
   },
 
 
