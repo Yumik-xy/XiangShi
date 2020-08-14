f1b50bd5dd3cdd5501b9199ad4e874c06468d602
Alaskan233 <65169013+Alaskan233@users.noreply.github.com>
2020-08-14 14:23:04 +0800

    资讯跳转完成 还有一个分类显示的bug

--- Wechatapplet/pages/index/index.js
+++ Wechatapplet/pages/index/index.js
@@ -1,6 +1,7 @@
 //index.js
 //获取应用实例
 const app = getApp()
+var time = require('../../utils/util.js');
 
 Page({
   data: {
@@ -68,7 +69,6 @@ Page({
         postid: 0,
         img_url: "../../icon/info/Img0.png",
         title: "乌鲁木齐：疫情“拐点”或正来临",
-        text: "新疆维吾尔自治区卫生健康委最新通报，8月8日0时至24时，新疆维吾尔自治区（含新疆生产建设兵团）报告新增新冠肺炎确诊病例15例，新增无症状感染者0例，均在乌鲁木齐市；新增治愈出院确诊病例30例，无症状感染者解除医学观察4例，均在乌鲁木齐市；重症转普通型2例，危重症转普通型1例。",
         readnum:"999",
         postwiki:"疫情",
         created:"2020-8-14"
@@ -78,7 +78,6 @@ Page({
         postid: 1,
         img_url: "../../icon/info/Img1.jpeg",
         title: "为什么疫情爆发要关闭海鲜市场",
-        text: "事实上，这也并不是海鲜的错，错在于人！ 一、货源不明 在国际著名的水产行业垂直网站SeaFoodsource（海鲜之源）一篇报道，根据2014年数据，现在中国市面上大约有30%的三文鱼都是通过走私途径来的。2018年，广州查获了一批价值6亿的三文鱼走私案，在报道中也提及这一次与三文鱼病毒有关的京深海鲜市场。在2016年，上海海关破获货值2.5亿特大海鲜走私案。",
         readnum:"999",
         postwiki:"疫情",
         created:"2020-8-14"
@@ -88,7 +87,6 @@ Page({
         postid: 2,
         img_url: "../../icon/info/Img2.jpg",
         title: "为什么现在年轻人脱发越来越严重了？",
-        text: "随着社会的不断发展，人们的生活作息、压力水平和饮食习惯都发生了巨大变化，脱发人群数量激增且呈年轻化发展，许多年轻人无奈的发现，原本以为脱发是中老年人的专利，离自己还很遥远，但是事实确并不是这回事----据世界卫生组织数据显示，中国男性脱发率达20%，30岁前脱发占比更是高达84%。同时调查发现，由于脱发对外在形象的负面影响，脱发人群较正常人群相比抑郁程度更高。",
         readnum:"999",
         postwiki:"百科",
         created:"2020-8-14"
@@ -98,7 +96,6 @@ Page({
         postid: 3,
         img_url: "../../icon/info/Img3.jpg",
         title: "大学生应该养成哪些好的生活习惯？",
-        text: "1.每天早上记得吃早饭无论再忙，时间再赶；或是再闲，起得晚点，也不要忘了吃早餐，哪怕吃上一点东西垫一垫肚子也好。每年大学生因为胃病请假、休学的不在少数。2.每天保持一定的运动量去健身房健身也好，在操场跑步也罢，或者只是每天饭后走走路消消食，都对身体有着不可或缺的好处。",
         readnum:"999",
         postwiki:"百科",
         created:"2020-8-14"
@@ -114,21 +111,18 @@ Page({
         postid: 0,
         img_url: "",
         title: "防疫就是命令，防控就是责任",
-        text: "防疫就是命令，防控就是责任"
       },
       {
         parent: 1,
         postid: 1,
         img_url: "",
         title: "防疫就是命令，防控就是责任",
-        text: "防疫就是命令，防控就是责任"
       },
       {
         parent: 1,
         postid: 2,
         img_url: "",
         title: "防疫就是命令，防控就是责任",
-        text: "防疫就是命令，防控就是责任"
       },
       ]
     },
@@ -141,21 +135,18 @@ Page({
         postid: 0,
         img_url: "",
         title: "防疫就是命令，防控就是责任",
-        text: "防疫就是命令，防控就是责任"
       },
       {
         parent: 2,
         postid: 1,
         img_url: "",
         title: "防疫就是命令，防控就是责任",
-        text: "防疫就是命令，防控就是责任"
       },
       {
         parent: 2,
         postid: 2,
         img_url: "",
         title: "防疫就是命令，防控就是责任",
-        text: "防疫就是命令，防控就是责任"
       },
       ]
     },
@@ -168,21 +159,18 @@ Page({
         postid: 0,
         img_url: "",
         title: "防疫就是命令，防控就是责任",
-        text: "防疫就是命令，防控就是责任"
       },
       {
         parent: 3,
         postid: 1,
         img_url: "",
         title: "防疫就是命令，防控就是责任",
-        text: "防疫就是命令，防控就是责任"
       },
       {
         parent: 3,
         postid: 2,
         img_url: "",
         title: "防疫就是命令，防控就是责任",
-        text: "防疫就是命令，防控就是责任"
       },
       ]
     },
@@ -205,6 +193,36 @@ Page({
     content.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false)
     this.setData({
       contents: content
+    })
+    var that = this
+    wx.request({
+      url: app.globalData.serverUrl + 'api/notify/list',
+      data: {
+        postwiki:index //获取点击的部分
+      },
+      header: {
+        "content-type": "application/x-www-form-urlencoded"
+      },
+      method: 'GET',
+      success: function (res) {
+        if (res.data.status == false) {
+          wx.showToast({
+            title: res.data.message,
+            icon: 'none'
+          })
+        } else if (res.data.status == true) {
+          var tempList = res.data.data
+          tempList.forEach((item) => {
+            item.created = time.formatTime(new Date(item.created*1000),'Y-M-D');
+            item.parent = 0
+          })
+
+          let item='contents['+index+'].content';
+          that.setData({
+            [item]: tempList //数据源
+          })
+        }
+      }
     })
   },
 
@@ -229,94 +247,14 @@ Page({
             icon: 'none'
           })
         } else if (res.data.status == true) {
+          console.log(res);
           var tempList = res.data.data
           tempList.forEach((item) => {
-            
+            item.created = time.formatTime(new Date(item.created*1000),'Y-M-D');
             item.parent = 0
           })
           that.setData({
-            'contents[0].content': tempList //数据源
-          })
-        }
-      }
-    })
-    wx.request({
-      url: app.globalData.serverUrl + 'api/notify/list',
-      data: {
-        postwiki:1 //获取热搜
-      },
-      header: {
-        "content-type": "application/x-www-form-urlencoded"
-      },
-      method: 'GET',
-      success: function (res) {
-        if (res.data.status == false) {
-          wx.showToast({
-            title: res.data.message,
-            icon: 'none'
-          })
-        } else if (res.data.status == true) {
-          var tempList = res.data.data
-          tempList.forEach((item) => {
-            
-            item.parent = 0
-          })
-          that.setData({
-            'contents[1].content': tempList //数据源
-          })
-        }
-      }
-    })
-    wx.request({
-      url: app.globalData.serverUrl + 'api/notify/list',
-      data: {
-        postwiki: 2 //获取热搜
-      },
-      header: {
-        "content-type": "application/x-www-form-urlencoded"
-      },
-      method: 'GET',
-      success: function (res) {
-        if (res.data.status == false) {
-          wx.showToast({
-            title: res.data.message,
-            icon: 'none'
-          })
-        } else if (res.data.status == true) {
-          var tempList = res.data.data
-          tempList.forEach((item) => {
-            
-            item.parent = 0
-          })
-          that.setData({
-            'contents[2].content': tempList //数据源
-          })
-        }
-      }
-    })
-    wx.request({
-      url: app.globalData.serverUrl + 'api/notify/list',
-      data: {
-        postwiki: 3 //获取热搜
-      },
-      header: {
-        "content-type": "application/x-www-form-urlencoded"
-      },
-      method: 'GET',
-      success: function (res) {
-        if (res.data.status == false) {
-          wx.showToast({
-            title: res.data.message,
-            icon: 'none'
-          })
-        } else if (res.data.status == true) {
-          var tempList = res.data.data
-          tempList.forEach((item) => {
-            
-            item.parent = 0
-          })
-          that.setData({
-            'contents[3].content': tempList //数据源
+            'contents[0].content': tempList 
           })
         }
       }
@@ -327,5 +265,4 @@ Page({
       isShowAll:!this.data.isShowAll
     })
   }
-
 })
--- Wechatapplet/utils/util.js
+++ Wechatapplet/utils/util.js
@@ -6,7 +6,8 @@ const formatTime = date => {
   const minute = date.getMinutes()
   const second = date.getSeconds()
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  return [year, month, day].map(formatNumber).join('-')
+  // + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
 const formatNumber = n => {
@@ -14,6 +15,26 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+function formatTimeTwo(number, format) {
+  var formateArr = ['Y', 'M', 'D', 'h', 'm', 's'];
+  var returnArr = [];
+
+  var date = new Date(number * 1000);
+  returnArr.push(date.getFullYear());
+  returnArr.push(formatNumber(date.getMonth() + 1));
+  returnArr.push(formatNumber(date.getDate()));
+
+  returnArr.push(formatNumber(date.getHours()));
+  returnArr.push(formatNumber(date.getMinutes()));
+  returnArr.push(formatNumber(date.getSeconds()));
+
+  for (var i in returnArr) {
+      format = format.replace(formateArr[i], returnArr[i]);
+  }
+  return format;
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  formatTimeTwo: formatTimeTwo
 }
